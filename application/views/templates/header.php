3e7e859bf466dd2b46de4926b2e27508db582558
lluuvvii <kurniawanluvi64@gmail.com>
2024-07-06 04:41:55 +0700

    update responsive

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,54 +4,56 @@
     <title>Sistem Informasi Pelayanan Publik</title>
     <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap.min.css'); ?>">
     <style>
-  .title-font {
-    font-family: 'Arial Black', sans-serif;
-    font-size: 1.5rem;
-    color: white;
-  }
-  .subtitle-font {
-    font-family: 'Arial', sans-serif;
-    font-size: 1rem;
-    color: white;
-  }
-</style>
+        .title-font {
+            font-family: 'Arial Black', sans-serif;
+            font-size: 1.5rem;
+            color: white;
+        }
+        .subtitle-font {
+            font-family: 'Arial', sans-serif;
+            font-size: 1rem;
+            color: white;
+        }
+    </style>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Bootstrap JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-primary bg-primary" data-bs-theme="dark">
-  <div class="mx-2">
-    <a class="navbar-brand" href="<?php echo base_url(); ?>">
-      <span class="title-font">SIPP !!</span><br>
-      <span class="subtitle-font">Sistem Informasi Pelayanan Publik</span>
-    </a>
-  </div>
-  <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo base_url('layanan'); ?>">Home</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="<?php echo base_url('layanan/create'); ?>">Tambah Data</a>
-          </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">
+            <span class="title-font">SIPP !!</span><br>
+            <span class="subtitle-font">Sistem Informasi Pelayanan Publik</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('layanan'); ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo base_url('layanan/create'); ?>">Tambah Data</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto">
                 <?php if ($this->session->userdata('logged_in')): ?>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('users/logout'); ?>">Logout</a>
-                        </li>
-                    <?php else: ?>
-                          <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('users/login'); ?>">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('users/register'); ?>">Register</a>
-                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url('users/logout'); ?>">Logout</a>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url('users/login'); ?>">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url('users/register'); ?>">Register</a>
+                    </li>
                 <?php endif; ?>
-                    </ul>
-  </div>
+            </ul>
+        </div>
+    </div>
 </nav>
 <div class="container">
-  
